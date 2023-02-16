d69af5b869da0d830287632e1dee9a0dcf94302f
Andrey <clashwef@gmail.com>
2023-02-16 17:33:09 +0300

    task4

---
+++ task4/script.js
@@ -0,0 +1,13 @@
+// Функция которая изменяет цвет исходя из атрибута value у option
+function changeColor(event) {
+    const select = event.target;
+    const value = select.value;
+    document.body.style.backgroundColor = value;
+}
+// Сюда пишите название события, которое должно произойти после смены option
+const selectColorEvent = 'change';
+// Создаем события на тег select.
+selectColor.addEventListener(selectColorEvent, changeColor);
+// Напишите, почему событие делаем на тег select, а работаем с тегом option?
+// Здесь писать.
+// ибо select являеться моделью со своими атрибутами, и из них мы можем узнать, какой чейчас выбран
